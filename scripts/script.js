b535ea93ad41b7baa17edab7dd6e00fc23b10dbc
Hmmam Babi Khouja <bhmmam@gmail.com>
2020-09-14 17:20:33 +0200

    PLAY SOUND AND MAKE PRESSING EFFECTS

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,18 @@
 const alphaButtons=document.querySelectorAll(".alphabet");
 
-alphaButtons.forEach(button => button.addEventListener("click",playSound))
+alphaButtons.forEach(button => button.addEventListener("click",pressBtn))
 
 const audios={};
 for (let i = 0; i < alphaButtons.length; i++) {
     audios[`audio${i}`] = new Audio(`../AR-alphabets/${i}.mp3`);
 }
 
-function playSound(){
+function pressBtn(){
     const num = Number(this.dataset.num);
+    alphaButtons[num].classList.add("btn-clicked");
     audios["audio"+num].play();
+    setTimeout(()=> {
+        alphaButtons[num].classList.remove("btn-clicked");
+    },750);
 }
-
 
