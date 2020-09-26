c959e04dd485727c339321ccd28cfaaa9d05ca3d
Hmmam Babi Khouja <bhmmam@gmail.com>
2020-09-26 09:20:00 +0200

    PLAY ALPHABETS SOUNDS
    after managing files profiles I had the error:
    
    "Uncaught (in promise) DOMException: Failed to load because no supported source was found"
    
    so I had to I had to change the playing sound mechanism

--- learn ar-alphabet/learn alphabet scripts/learn-alpha.js
+++ learn ar-alphabet/learn alphabet scripts/learn-alpha.js
@@ -2,17 +2,11 @@ const alphaButtons=document.querySelectorAll(".alphabet");
 
 alphaButtons.forEach(button => button.addEventListener("click",pressBtn))
 
-const audios={};
-for (let i = 0; i < alphaButtons.length; i++) {
-    audios[`audio${i}`] = new Audio(`../AR-alphabets/${i}.mp3`);
-}
-
-function pressBtn(){
-    const num = Number(this.dataset.num);
+function play(num){
+    let audio = document.getElementById("aud"+num);
+    audio.play();
     alphaButtons[num].classList.add("btn-clicked");
-    audios["audio"+num].play();
     setTimeout(()=> {
         alphaButtons[num].classList.remove("btn-clicked");
     },750);
 }
-
