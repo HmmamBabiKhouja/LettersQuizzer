4d01586473000b29e2c9f15dd10d83e076a3cac6
Hmmam Babi Khouja <bhmmam@gmail.com>
2020-10-06 06:09:45 +0200

    ADD WIN/LOSE SOUND EFFECTS TO THE GAME
    added win/ lose sound effects that plays with each round
    
    I resorted sounds and sound files on resources

--- choose the right/choose the right scripts/choose-game.js
+++ choose the right/choose the right scripts/choose-game.js
@@ -8,7 +8,10 @@ const audioPlayer = document.querySelector("#audio-tag");
 const darkPlayTriangle = document.querySelector(".dark-triangle");
 const lightPlayTriangle = document.querySelector(".light-triangle");
 
-// PRIMITIVES VARIABLES
+// SOUND VARIABLES
+const wonRound= new Audio("../../resources/effect-sounds/won.mp3");
+const lostRound= new Audio("../../resources/effect-sounds/lost.mp3");
+// PRIMITIVE VARIABLES
 let score = 0;
 const alphabets = ["أ","ب","ت","ث","ج","ح","خ","د","ذ","ر","ز","س","ش","ص",
                     "ض","ط","ظ","ع","غ","ف","ق","ك","ل","م","ن","ه","و","ي"];
@@ -31,7 +34,7 @@ function init(){
 }
 
 function playLetter(){
-    let path = `../../resources/AR-alphabets/${randomNum}.mp3`;
+    let path = `../../resources/AR-alphabets-sounds/${randomNum}.mp3`;
     audioPlayer.src=path;
     audioPlayer.play();
     darkPlayTriangle.classList.add("dark-triangle-clicked");
@@ -43,7 +46,7 @@ function playLetter(){
 }
 
 function checkAnswer(answer){
-    
+    let sound;
     letterChoices[answer].classList.add("btn-clicked");
     setTimeout(()=>{
         letterChoices[answer].classList.remove("btn-clicked");
@@ -52,15 +55,18 @@ function checkAnswer(answer){
     let answerValue= letterChoices[answer].innerHTML;
     if(answerValue===alphabets[randomNum]){
         score++;
+        sound=wonRound;
     }else{
+        sound=lostRound;
         score=0;
     }
+    sound.play();
 
-    let arabicScore=EntoAr(score.toString());
-    scoreDisplay.innerHTML=arabicScore;
+    let arabicScore=enToAr(score.toString());
     setTimeout(()=>{
         init();
-    },1200)
+        scoreDisplay.innerHTML=arabicScore;
+    },1300)
 }
 
 //English to Arabic digits.
